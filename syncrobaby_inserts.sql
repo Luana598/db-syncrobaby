e366d31574866227c6edb71624dd54a202aa6297
Luana598 <marianaluana598@gmail.com>
2026-05-26 11:13:56 -0300

    eventos concluídos

--- syncrobaby_inserts.sql
+++ syncrobaby_inserts.sql
@@ -1,9 +1,70 @@
 USE db_syncrobaby;
 
+INSERT INTO tbl_vaccine (vaccine_name, observation, prevented_diseases, dose) VALUES
+-- Ao nascer
+('Vacina BCG', NULL, 'Formas graves e disseminadas da tuberculose e, também, com efeito protetor contra a hanseníase', 'dose única'),
+('Vacina Hepatite B', NULL, 'Hepatite B e hepatite D', '1 dose'),
+
+-- 2 meses
+('Vacina penta (DTP+Hib+HB)', NULL, 'Difteria, tétano, coqueluche, infecções causadas por Haemophilus influenzae B e hepatite B', '1ª dose'),
+('Vacina poliomielite inativada VIP', NULL, 'Poliomielite (paralisia infantil)', '1ª dose'),
+('Vacina pneumocócica 10-valente', NULL, 'Doenças pneumocócicas invasivas pelos sorotipos contidos na vacina', '1ª dose'),
+('Vacina rotavírus humano', 'Atenção aos prazos para esta vacinação, a 1ª dose deve ser aplicada entre 1 mês e 15 dias e 11 meses e 29 dias. A 2ª dose, entre 3 meses e 15 dias e 23 meses e 29 dias. Se a 1ª dose não for feita no período indicado, a criança perderá a oportunidade da 2ª dose.', 'Gastrenterite viral (diarreia e vômitos)', '1ª dose'),
+
+-- 3 meses
+('Vacina meningocócica C', NULL, 'Doenças meningocócicas (meningite, encefalite, meningoencefalite) pelo meningococo tipo C', '1ª dose'),
+
+-- 4 meses
+('Vacina penta (DTP+Hib+HB)', NULL, 'Difteria, tétano, coqueluche, infecções causadas por Haemophilus influenzae B e hepatite B', '2ª dose'),
+('Vacina poliomielite inativada VIP', NULL, 'Poliomielite (paralisia infantil)', '2ª dose'),
+('Vacina pneumocócica 10-valente', NULL, 'Doenças pneumocócicas invasivas pelos sorotipos contidos na vacina', '2ª dose'),
+('Vacina rotavírus humano', 'Obs.: Atenção aos prazos para esta vacinação, a 1ª dose deve ser aplicada entre 1 mês e 15 dias e 11 meses e 29 dias. A 2ª dose, entre 3 meses e 15 dias e 23 meses e 29 dias. Se a 1ª dose não for feita no período indicado, a criança perderá a oportunidade da 2ª dose.', 'Gastrenterite viral (diarreia e vômitos)', '2ª dose'),
+
+-- 5 meses
+('Vacina meningocócica C', NULL, 'Doenças meningocócicas (meningite, encefalite, meningoencefalite) pelo meningococo tipo C', '2ª dose'),
+
+-- 6 meses
+('Vacina penta (DTP+Hib+HB)', NULL, 'Difteria, tétano, coqueluche, infecções causadas por Haemophilus influenzae B e hepatite B', '3ª dose'),
+('Vacina poliomielite inativada VIP', NULL, 'Poliomielite (paralisia infantil)', '3ª dose'),
+('Vacina influenza trivalente', 'Obs.: Crianças de 6 meses a menores de 6 anos devem ser vacinadas todo ano. Quem vai receber a vacina pela primeira vez deve tomar 2 doses com 30 dias de intervalo. As que já tomaram em anos anteriores recebem apenas 1 dose por ano.', 'Gripe (influenza)', '1ª dose'),
+('Vacina covid-19', 'Obs.: O esquema pode ser de 2 doses (6 e 7 meses, vacina Spikevax) ou 3 doses (6, 7 e 9 meses, vacina Comirnaty). Para crianças imunocomprometidas, 3 doses, com reforço a cada 6 meses até os 4 anos.', 'Formas graves e óbitos causados pelo vírus SARS-CoV-2', '1ª dose'),
+
+-- 6 a 8 meses
+('Vacina febre amarela', 'Obs.: A vacina contra febre amarela pode ser recomendada para esta idade quando há alto risco de contrair a doença e não é possível adiar a vacinação. Isso vale para quem vive ou vai viajar para áreas com transmissão ativa, sempre após avaliação do serviço de saúde. Para viajantes, a vacina deve ser tomada pelo menos 10 dias antes da viagem', 'Febre amarela e suas complicações', '1 dose'),
+
+-- 7 meses
+('Vacina covid-19', 'Obs.: O esquema pode ser de 2 doses (6 e 7 meses, vacina Spikevax) ou 3 doses (6, 7 e 9 meses, vacina Comirnaty). Para crianças imunocomprometidas, 3 doses, com reforço a cada 6 meses até os 4 anos.', 'Formas graves e óbitos causados pelo vírus SARS-CoV-2', '2ª dose'),
+
+-- 9 meses
+('Vacina covid-19', 'Obs.: O esquema pode ser de 2 doses (6 e 7 meses, vacina Spikevax) ou 3 doses (6, 7 e 9 meses, vacina Comirnaty). Para crianças imunocomprometidas, 3 doses, com reforço a cada 6 meses até os 4 anos.', 'Formas graves e óbitos causados pelo vírus SARS-CoV-2', '3ª dose'),
+('Vacina febre amarela', 'Obs.: Manter a situação vacinal atualizada, principalmente para os residentes e viajantes para área com transmissão ativa da doença. Para os viajantes, a vacina deve ser tomada pelo menos 10 dias antes da viagem.', 'Febre amarela e suas complicações', '1ª dose'),
+
+-- 12 meses
+('Vacina pneumocócica 10-valente', NULL, 'Doenças pneumocócicas invasivas pelos sorogrupos contidos na vacina', '1 dose reforço'),
+('Vacina meningocócica ACWY', NULL, 'Doenças meningocócicas (meningite, encefalite, meningoencefalite) por meningococos do tipo A, C, W e Y', '1 dose'),
+('Vacina tríplice viral SCR', NULL, 'Sarampo, caxumba, rubéola e síndrome da rubéola congênita (futuramente, na gravidez)', '1 dose'),
+
+-- 15 meses
+('Vacina DTP', NULL, 'Difteria, tétano e coqueluche', '1ª dose reforço'),
+('Vacina poliomielite inativada VIP', NULL, 'Poliomielite (paralisia infantil)', '1ª dose reforço'),
+('Vacina tríplice viral SCR', 'Obs.: Em casos de indisponibilidade da vacina varicela monovalente, a vacina tetraviral poderá ser utilizada.', 'Sarampo, caxumba, rubéola, síndrome da rubéola congênita (futuramente, na gravidez)', '2ª dose'),
+('Vacina Varicela', 'Obs.: Em casos de indisponibilidade da vacina varicela monovalente, a vacina tetraviral poderá ser utilizada.', 'Varicela (catapora)', '1ª dose'),
+('Vacina hepatite A', NULL, 'Hepatite A', '1ª dose'),
+
+-- 4 anos
+('Vacina DTP', 'Obs.: Recomenda-se, em sequência, manter 1 dose de reforço com dT a cada 10 anos após a última dose DTP, antecipando para 5 anos em caso de exposição ao risco de tétano ou difteria.', 'Difteria, tétano, coqueluche', '2º reforço'),
+('Vacina febre amarela', 'Obs.: Manter a situação vacinal atualizada, principalmente para os residentes e viajantes para área com transmissão ativa da doença. Para os viajantes, a vacina deve ser tomada pelo menos 10 dias antes da viagem.', 'Febre amarela', '1 dose reforço'),
+('Vacina varicela', NULL, 'Varicela (catapora)', '1 dose'),
+
+-- 5 anos
+('Vacina pneumocócica 23-valente', 'Obs.: Sem histórico vacinal com pneumo conjugada. Uma segunda dose deve ser administrada com intervalo de 5 anos após a 1ª dose.', 'Doenças pneumocócicas invasivas pelos sorogrupos contidos na vacina', '1 dose');
+
+
 INSERT INTO tbl_guardian (guardian_name, email, password, profile_picture) VALUES 
 ('Ana Souza', 'ana.souza@email.com', 'senha123', 'perfil_ana.jpg'),
 ('Carlos Lima', 'carlos.lima@email.com', 'carlos456', 'perfil_carlos.jpg'),
 ('Mariana Silva', 'mari.silva@email.com', 'mari789', NULL);
+
 
 INSERT INTO tbl_child (child_name, height, weight, birth_date, BMI, blood_type, gender, fk_id_guardian) VALUES 
 ('Alice Souza', 52.0, 3.50, '2023-05-10', 12.9, 'O+', 'female', 1),
@@ -284,65 +345,6 @@ INSERT INTO tbl_stock_diaper
 VALUES 
 (1, 1, 1);
 
-INSERT INTO tbl_vaccine (vaccine_name, observation, prevented_diseases, dose) VALUES
--- Ao nascer
-('Vacina BCG', NULL, 'Formas graves e disseminadas da tuberculose e, também, com efeito protetor contra a hanseníase', 'dose única'),
-('Vacina Hepatite B', NULL, 'Hepatite B e hepatite D', '1 dose'),
-
--- 2 meses
-('Vacina penta (DTP+Hib+HB)', NULL, 'Difteria, tétano, coqueluche, infecções causadas por Haemophilus influenzae B e hepatite B', '1ª dose'),
-('Vacina poliomielite inativada VIP', NULL, 'Poliomielite (paralisia infantil)', '1ª dose'),
-('Vacina pneumocócica 10-valente', NULL, 'Doenças pneumocócicas invasivas pelos sorotipos contidos na vacina', '1ª dose'),
-('Vacina rotavírus humano', 'Atenção aos prazos para esta vacinação, a 1ª dose deve ser aplicada entre 1 mês e 15 dias e 11 meses e 29 dias. A 2ª dose, entre 3 meses e 15 dias e 23 meses e 29 dias. Se a 1ª dose não for feita no período indicado, a criança perderá a oportunidade da 2ª dose.', 'Gastrenterite viral (diarreia e vômitos)', '1ª dose'),
-
--- 3 meses
-('Vacina meningocócica C', NULL, 'Doenças meningocócicas (meningite, encefalite, meningoencefalite) pelo meningococo tipo C', '1ª dose'),
-
--- 4 meses
-('Vacina penta (DTP+Hib+HB)', NULL, 'Difteria, tétano, coqueluche, infecções causadas por Haemophilus influenzae B e hepatite B', '2ª dose'),
-('Vacina poliomielite inativada VIP', NULL, 'Poliomielite (paralisia infantil)', '2ª dose'),
-('Vacina pneumocócica 10-valente', NULL, 'Doenças pneumocócicas invasivas pelos sorotipos contidos na vacina', '2ª dose'),
-('Vacina rotavírus humano', 'Obs.: Atenção aos prazos para esta vacinação, a 1ª dose deve ser aplicada entre 1 mês e 15 dias e 11 meses e 29 dias. A 2ª dose, entre 3 meses e 15 dias e 23 meses e 29 dias. Se a 1ª dose não for feita no período indicado, a criança perderá a oportunidade da 2ª dose.', 'Gastrenterite viral (diarreia e vômitos)', '2ª dose'),
-
--- 5 meses
-('Vacina meningocócica C', NULL, 'Doenças meningocócicas (meningite, encefalite, meningoencefalite) pelo meningococo tipo C', '2ª dose'),
-
--- 6 meses
-('Vacina penta (DTP+Hib+HB)', NULL, 'Difteria, tétano, coqueluche, infecções causadas por Haemophilus influenzae B e hepatite B', '3ª dose'),
-('Vacina poliomielite inativada VIP', NULL, 'Poliomielite (paralisia infantil)', '3ª dose'),
-('Vacina influenza trivalente', 'Obs.: Crianças de 6 meses a menores de 6 anos devem ser vacinadas todo ano. Quem vai receber a vacina pela primeira vez deve tomar 2 doses com 30 dias de intervalo. As que já tomaram em anos anteriores recebem apenas 1 dose por ano.', 'Gripe (influenza)', '1ª dose'),
-('Vacina covid-19', 'Obs.: O esquema pode ser de 2 doses (6 e 7 meses, vacina Spikevax) ou 3 doses (6, 7 e 9 meses, vacina Comirnaty). Para crianças imunocomprometidas, 3 doses, com reforço a cada 6 meses até os 4 anos.', 'Formas graves e óbitos causados pelo vírus SARS-CoV-2', '1ª dose'),
-
--- 6 a 8 meses
-('Vacina febre amarela', 'Obs.: A vacina contra febre amarela pode ser recomendada para esta idade quando há alto risco de contrair a doença e não é possível adiar a vacinação. Isso vale para quem vive ou vai viajar para áreas com transmissão ativa, sempre após avaliação do serviço de saúde. Para viajantes, a vacina deve ser tomada pelo menos 10 dias antes da viagem', 'Febre amarela e suas complicações', '1 dose'),
-
--- 7 meses
-('Vacina covid-19', 'Obs.: O esquema pode ser de 2 doses (6 e 7 meses, vacina Spikevax) ou 3 doses (6, 7 e 9 meses, vacina Comirnaty). Para crianças imunocomprometidas, 3 doses, com reforço a cada 6 meses até os 4 anos.', 'Formas graves e óbitos causados pelo vírus SARS-CoV-2', '2ª dose'),
-
--- 9 meses
-('Vacina covid-19', 'Obs.: O esquema pode ser de 2 doses (6 e 7 meses, vacina Spikevax) ou 3 doses (6, 7 e 9 meses, vacina Comirnaty). Para crianças imunocomprometidas, 3 doses, com reforço a cada 6 meses até os 4 anos.', 'Formas graves e óbitos causados pelo vírus SARS-CoV-2', '3ª dose'),
-('Vacina febre amarela', 'Obs.: Manter a situação vacinal atualizada, principalmente para os residentes e viajantes para área com transmissão ativa da doença. Para os viajantes, a vacina deve ser tomada pelo menos 10 dias antes da viagem.', 'Febre amarela e suas complicações', '1ª dose'),
-
--- 12 meses
-('Vacina pneumocócica 10-valente', NULL, 'Doenças pneumocócicas invasivas pelos sorogrupos contidos na vacina', '1 dose reforço'),
-('Vacina meningocócica ACWY', NULL, 'Doenças meningocócicas (meningite, encefalite, meningoencefalite) por meningococos do tipo A, C, W e Y', '1 dose'),
-('Vacina tríplice viral SCR', NULL, 'Sarampo, caxumba, rubéola e síndrome da rubéola congênita (futuramente, na gravidez)', '1 dose'),
-
--- 15 meses
-('Vacina DTP', NULL, 'Difteria, tétano e coqueluche', '1ª dose reforço'),
-('Vacina poliomielite inativada VIP', NULL, 'Poliomielite (paralisia infantil)', '1ª dose reforço'),
-('Vacina tríplice viral SCR', 'Obs.: Em casos de indisponibilidade da vacina varicela monovalente, a vacina tetraviral poderá ser utilizada.', 'Sarampo, caxumba, rubéola, síndrome da rubéola congênita (futuramente, na gravidez)', '2ª dose'),
-('Vacina Varicela', 'Obs.: Em casos de indisponibilidade da vacina varicela monovalente, a vacina tetraviral poderá ser utilizada.', 'Varicela (catapora)', '1ª dose'),
-('Vacina hepatite A', NULL, 'Hepatite A', '1ª dose'),
-
--- 4 anos
-('Vacina DTP', 'Obs.: Recomenda-se, em sequência, manter 1 dose de reforço com dT a cada 10 anos após a última dose DTP, antecipando para 5 anos em caso de exposição ao risco de tétano ou difteria.', 'Difteria, tétano, coqueluche', '2º reforço'),
-('Vacina febre amarela', 'Obs.: Manter a situação vacinal atualizada, principalmente para os residentes e viajantes para área com transmissão ativa da doença. Para os viajantes, a vacina deve ser tomada pelo menos 10 dias antes da viagem.', 'Febre amarela', '1 dose reforço'),
-('Vacina varicela', NULL, 'Varicela (catapora)', '1 dose'),
-
--- 5 anos
-('Vacina pneumocócica 23-valente', 'Obs.: Sem histórico vacinal com pneumo conjugada. Uma segunda dose deve ser administrada com intervalo de 5 anos após a 1ª dose.', 'Doenças pneumocócicas invasivas pelos sorogrupos contidos na vacina', '1 dose');
-
 
 INSERT INTO tbl_age_group (age_group_name, min_months, max_months) VALUES 
 ('Recém nascido', 0, 1), 
@@ -423,3 +425,4 @@ INSERT INTO tbl_vaccine_in_age_group (fk_id_age_group, fk_id_vaccine) VALUES
 
 -- 5 anos
 (17, 32);
+
--- syncrobaby_selects.sql
+++ syncrobaby_selects.sql
@@ -30,7 +30,7 @@ LEFT JOIN tbl_child c
     ON cv.fk_id_child = c.id_child;
     
     
-    SELECT * FROM vw_vaccination_status where id_age_group = 3 and id_child = 4;
+    SELECT * FROM vw_vaccination_status where id_child = 1;
     
 
 -- 2. VIEW para linha temporal do dia - rotinas
@@ -182,4 +182,5 @@ INNER JOIN tbl_age_group ag
     
     SELECT * FROM vw_article_by_age_group ORDER BY fk_id_age_group;
     
+    select * from tbl_child_vaccine;
     
